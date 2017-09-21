eb0de87622d9aa54e3cfa3c18d064de12ad13cb5
GR <linux2.0@foxmail.com>
2017-09-21 17:16:11 +0800

    Initial commit

--- app/src/main/java/me/gr/fishdrawable/FishDrawable.kt
+++ app/src/main/java/me/gr/fishdrawable/FishDrawable.kt
@@ -7,6 +7,7 @@ import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.util.TypedValue
+import android.view.animation.LinearInterpolator
 import java.util.*
 
 
@@ -18,12 +19,13 @@ class FishDrawable : Drawable() {
     private val BODY_LENGTH = HEAD_RADIUS * 3.2f
     private val FINS_LENGTH = HEAD_RADIUS * 1.3f
     private val TAIL1_LENGTH = HEAD_RADIUS * 1.12f
+    private val TAIL2_LENGTH = HEAD_RADIUS * 1.3f
     private val TOTAL_LENGTH = HEAD_RADIUS * 6.79f
     private val BODY_COLOR = Color.argb(220, 244, 92, 71)
     private val FINS_COLOR = Color.argb(100, 244, 92, 71)
     private val OTHER_COLOR = Color.argb(160, 244, 92, 71)
     private val FINS_ANGLE = 115f
-    private val SWAY_DURATION = 180_000
+    private val SWAY_DURATION = 180_000L
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
     private val path = Path()
@@ -32,16 +34,29 @@ class FishDrawable : Drawable() {
     private var middlePoint = PointF(HEAD_RADIUS * 4.18f, HEAD_RADIUS * 4.18f)
     private lateinit var endBodyPoint: PointF
     private lateinit var endTail1Point: PointF
+    private lateinit var endTail2Point: PointF
     private var mainAngle = 90f
     private var bodyAngle = 0f
     private var finsAngle = 0f
     private var tail1Angle = 0f
+    private var tail2Angle = 0f
     private var waveFrequency = 1f
     private var currentValue = 0
 
     init {
         finsAnimator.repeatMode = ValueAnimator.REVERSE
         finsAnimator.repeatCount = Random().nextInt(3)
+
+        val animator = ValueAnimator.ofInt(0, 54000)
+        animator.duration = SWAY_DURATION
+        animator.interpolator = LinearInterpolator()
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.repeatMode = ValueAnimator.REVERSE
+        animator.addUpdateListener {
+            currentValue = it.animatedValue as Int
+            invalidateSelf()
+        }
+        animator.start()
     }
 
     override fun draw(canvas: Canvas) {
@@ -142,19 +157,49 @@ class FishDrawable : Drawable() {
     }
 
     private fun drawTail2(canvas: Canvas) {
+        val point1 = calculatePointF(endTail1Point, HEAD_RADIUS * 0.42f, tail2Angle - 90)
+        val point2 = calculatePointF(endTail2Point, HEAD_RADIUS * 0.168f, tail2Angle - 90)
+        val point3 = calculatePointF(endTail2Point, HEAD_RADIUS * 0.168f, tail2Angle + 90)
+        val point4 = calculatePointF(endTail1Point, HEAD_RADIUS * 0.42f, tail2Angle + 90)
 
+        path.reset()
+        path.moveTo(point1.x, point1.y)
+        path.lineTo(point2.x, point2.y)
+        path.lineTo(point3.x, point3.y)
+        path.lineTo(point4.x, point4.y)
+        canvas.drawPath(path, paint)
     }
 
     private fun drawTail3(canvas: Canvas) {
+        val width = Math.abs(Math.sin(Math.toRadians(currentValue * waveFrequency * 1.7)) * HEAD_RADIUS * 0.42f + HEAD_RADIUS / 5f * 3f).toFloat()
+        val endPoint1 = calculatePointF(endTail1Point, TAIL2_LENGTH, tail2Angle - 180)
+        val endPoint2 = calculatePointF(endTail1Point, TAIL2_LENGTH - dp2px(3f), tail2Angle - 180)
+        val point1 = calculatePointF(endPoint1, width, tail2Angle - 90)
+        val point2 = calculatePointF(endPoint1, width, tail2Angle + 90)
+        val point3 = calculatePointF(endPoint2, width - dp2px(6f), tail2Angle - 90)
+        val point4 = calculatePointF(endPoint2, width - dp2px(6f), tail2Angle + 90)
 
+        path.reset()
+        path.moveTo(endTail1Point.x, endTail1Point.y)
+        path.lineTo(point3.x, point3.y)
+        path.lineTo(point4.x, point4.y)
+        canvas.drawPath(path, paint)
+
+        path.reset()
+        path.moveTo(endTail1Point.x, endTail1Point.y)
+        path.lineTo(point1.x, point1.y)
+        path.lineTo(point2.x, point2.y)
+        canvas.drawPath(path, paint)
     }
 
     private fun calculateValue() {
         bodyAngle = mainAngle + (Math.sin(Math.toRadians(currentValue * waveFrequency * 1.2)) * 2).toFloat()
         headPoint = calculatePointF(middlePoint, BODY_LENGTH / 2, mainAngle)
         endBodyPoint = calculatePointF(headPoint, BODY_LENGTH, bodyAngle - 180)
-        tail1Angle = bodyAngle + (Math.cos(Math.toRadians(currentValue * waveFrequency * 1.5))).toFloat() * 15f - 180f
-        endTail1Point = calculatePointF(endBodyPoint, TAIL1_LENGTH, tail1Angle)
+        tail1Angle = bodyAngle + (Math.cos(Math.toRadians(currentValue * waveFrequency * 1.5))).toFloat() * 15f
+        endTail1Point = calculatePointF(endBodyPoint, TAIL1_LENGTH, tail1Angle - 180f)
+        tail2Angle = bodyAngle + (Math.sin(Math.toRadians(currentValue * waveFrequency * 1.5))).toFloat() * 35f
+        endTail2Point = calculatePointF(endTail1Point, TAIL2_LENGTH, tail2Angle - 180f)
     }
 
     private fun calculatePointF(startPointF: PointF, length: Float, angle: Float): PointF {
