fd8363d46cca0eaf0216c3537905b39199ac0d38
GR <linux2.0@foxmail.com>
2017-10-21 19:12:17 +0800

    Initial commit

--- app/src/main/java/me/gr/fishdrawable/Ext.kt
+++ app/src/main/java/me/gr/fishdrawable/Ext.kt
@@ -9,7 +9,6 @@ import android.util.TypedValue
  */
 fun dp2px(dp: Float): Float = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().displayMetrics)
 
-
 fun calculatePointF(startPointF: PointF, length: Float, angle: Float): PointF {
     val deltaX = Math.cos(Math.toRadians(angle.toDouble())) * length
     val deltaY = Math.sin(Math.toRadians(angle.toDouble() - 180)) * length
